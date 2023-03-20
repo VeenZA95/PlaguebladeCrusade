b90277d393184bd25a5775cd31befa4fa5d56579
VeenZA95 <dylanbowes95@gmail.com>
2023-03-20 13:03:54 +0200

    Added Random Word Spawner

---
+++ Assets/Scripts/WordDisplay.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+
+public class WordDisplay : MonoBehaviour
+{
+
+    public TextMeshProUGUI text;
+    public float fallSpeed = 1f;
+
+    public void SetWord(string word)
+    {
+        text.text = word;
+    }
+
+    public void RemoveLetter()
+    {
+        text.text = text.text.Remove(0, 1);
+        text.color = Color.red;
+    }
+
+    public void RemoveWord()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Translate(0f, -fallSpeed * Time.deltaTime, 0f);
+    }
+
+}
